9bc4c331dc64a3840ff42b1c9b7e8dfead670241
Aditya <savarkaraditya7@gmail.com>
2025-11-19 16:47:54 -0600

    Add tennis data scraping script

---
+++ MatchPredictor.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pytorch
+import pytorch
+
+url = "https://raw.githubusercontent.com/JeffSackmann/tennis_atp/refs/heads/master/atp_matches_2024.csv"
+df = pd.read_csv(url)
+
+
+    
+
+
